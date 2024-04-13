85d1fc1d33aec019770398441e9d5b33491278f4
Ville Hänninen <ville.hanninen94@gmail.com>
2024-04-13 23:06:30 +0300

    exercise 10

--- src/theme.js
+++ src/theme.js
@@ -1,24 +1,30 @@
+import { Platform } from 'react-native';
+
 const theme = {
-    colors: {
-      textPrimary: '#24292e',
-      textSecondary: '#586069',
-      primary: '#0366d6',
-      appBarBackground: '#24292e',
-      appBarText: 'white',
-      white: 'white',
-      mainBackground: '#e1e4e8',
-    },
-    fontSizes: {
-      body: 14,
-      subheading: 16,
-    },
-    fonts: {
-      main: 'System',
-    },
-    fontWeights: {
-      normal: '400',
-      bold: '700',
-    },
-  };
-  
-  export default theme;
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+    appBarBackground: '#24292e',
+    appBarText: 'white',
+    white: 'white',
+    mainBackground: '#e1e4e8',
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+  },
+  fonts: {
+    color: Platform.select({
+      android: 'Roboto',
+      ios: 'Arial',
+      default: 'System',
+    }),
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+};
+
+export default theme;
