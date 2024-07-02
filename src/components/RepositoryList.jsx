42c139470a65c39a1965b5f59e1043220e48a063
Ville Hänninen <ville.hanninen94@gmail.com>
2024-07-03 00:03:16 +0300

    exercise 24

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,28 @@
-import { FlatList, View, StyleSheet, Pressable } from 'react-native';
+import { FlatList, View, StyleSheet, Pressable, TextInput } from 'react-native';
 import RepositoryItem from './RepositoryItem';
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORIES } from '../graphql/queries';
 import { useNavigate } from 'react-router-native';
 import { useState } from 'react';
 import { Button, Menu, Divider, Provider as PaperProvider } from 'react-native-paper';
+import { useDebounce } from 'use-debounce';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
+  },  
+  searchInput: {
+    height: 50,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 8,
+    paddingHorizontal: 8,
   },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-const RepositoryListContainer = ({ repositories, setOrder, setOrderDirection }) => {
+const RepositoryListContainer = ({ repositories, setOrder, setOrderDirection, searchKeyword, setSearchKeyword }) => {
   const navigate = useNavigate();
 
   const repositoryNodes = repositories
@@ -23,6 +31,14 @@ const RepositoryListContainer = ({ repositories, setOrder, setOrderDirection }) 
 
   return (
     <PaperProvider>
+            <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          value={searchKeyword}
+          placeholder="Search"
+          onChangeText={(text) => setSearchKeyword(text)}
+        />
+      </View>
       <FlatList
         ListHeaderComponent={<OrderSelection setOrder={setOrder} setOrderDirection={setOrderDirection} />}
         data={repositoryNodes}
@@ -42,12 +58,15 @@ const RepositoryListContainer = ({ repositories, setOrder, setOrderDirection }) 
 const RepositoryList = () => {
   const [order, setOrder] = useState('CREATED_AT');
   const [orderDirection, setOrderDirection] = useState('DESC');
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [debouncedSearchKeyword] = useDebounce(searchKeyword, 500);
 
   const { data, error, loading } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
     variables: {
       orderBy: order,
       orderDirection: orderDirection,
+      searchKeyword: debouncedSearchKeyword,
     },
   });
 
@@ -58,6 +77,8 @@ const RepositoryList = () => {
       repositories={data.repositories}
       setOrder={setOrder}
       setOrderDirection={setOrderDirection}
+      searchKeyword={searchKeyword}
+      setSearchKeyword={setSearchKeyword}
     />
   );
 };
